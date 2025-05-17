e953982850953f537be290a2e906b1dcf108590d
Frenk012 <francescocaccavo75@gmail.com>
2025-05-17 16:06:17 +0200

    Remove unused pandas import from streamlit_app.py

--- backend/streamlit_app.py
+++ backend/streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from manager import calculate_processing, get_ingredients
-import pandas as pd
 
 potions_elements = ["health di tier 0", "health di tier 1", "health di tier 2", "anthidot di tier 1", "anthidot di tier 2", "mending di tier 0", "mending di tier 1", "mending di tier 2",
      "slowness", "swiftness di tier 1", "swiftness di tier 2", "slow_fall", "jump1", "jump di tier 2", "weakness",
